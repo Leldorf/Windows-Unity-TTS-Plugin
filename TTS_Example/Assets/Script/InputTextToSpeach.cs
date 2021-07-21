3e7552782a010c3d23545ee4f9ffee528341c4e0
JaeHun Lee <86466517+Leldorf@users.noreply.github.com>
2021-07-21 20:07:31 +0900

    Add files via upload

---
+++ TTS_Example/Assets/Script/InputTextToSpeach.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class InputTextToSpeach : MonoBehaviour
+{
+    [SerializeField]
+    Text input = null;
+    [SerializeField]
+    TTS tts = null;
+
+    private void Awake()
+    {
+        if (input == null)
+            input = transform.GetChild(1).GetComponent<Text>();
+        if (tts == null)
+            tts = GameObject.Find("TTS").GetComponent<TTS>();
+    }
+
+    public void SpeechText()
+    {
+        if(input.text.Length != 0)
+            tts.Speech(input.text);
+    }
+}
---
+++ TTS_Example/Assets/Script/VoiceSelector.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class VoiceSelector : MonoBehaviour
+{
+    [SerializeField]
+    TTS tts = null;
+    Dropdown selector;
+    // Start is called before the first frame update
+    void Start()
+    {
+        selector = GetComponent<Dropdown>();
+
+        if(tts == null)
+        {
+            tts = GameObject.Find("TTS").GetComponent<TTS>();
+        }
+        List<string> names = tts.GetVoiceNmaes();
+
+        foreach(string name in names)
+        {
+            Dropdown.OptionData option = new Dropdown.OptionData();
+            option.text = name;
+            selector.options.Add(option);
+        }
+        selector.value = names.Count-1;
+        SelectVoice();
+    }
+    public void SelectVoice()
+    {
+        Debug.Log(selector.value);
+        tts.SetVoice(selector.value);
+    }
+}
